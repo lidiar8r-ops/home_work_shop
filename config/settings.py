64bf189b1c8be5517202815d66a34418606aae24
Рыбина Лидия <lidiar@rambler.ru>
2026-02-03 22:05:05 +0500

    update settings and add .env.sample

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,6 @@ load_dotenv(override=True)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY= os.getenv('SECRET_KEY')
 
