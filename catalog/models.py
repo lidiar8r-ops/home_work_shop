c842357f4b086c8ed47498bf6e9cf28a80c4b80d
Рыбина Лидия <lidiar@rambler.ru>
2026-02-08 13:23:07 +0500

    update models and requirements.txt and env.sample

--- catalog/models.py
+++ catalog/models.py
@@ -11,17 +11,17 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
-        ordering = ['name']
+        ordering = ['name',]
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name="Наименование")
     description = models.TextField(max_length=1000, verbose_name="Описание")
     image = models.ImageField(upload_to='images/', verbose_name="Изображение")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='categories')
     price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name="Цена за покупку")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
-    updated_at = models.DateTimeField(auto_now=True, erbose_name="Дата последнего изменения")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
 
     def __str__(self):
         return self.name
@@ -29,4 +29,4 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'книга'
         verbose_name_plural = 'книги'
-        ordering = ['title']
+        ordering = ['title',]
