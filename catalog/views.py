1a4bc6ba30eb2743a5308eab90ee1ff8a035dce7
Рыбина Лидия <lidiar@rambler.ru>
2026-03-03 22:42:20 +0500

    update product_list home и views.py

--- catalog/views.py
+++ catalog/views.py
@@ -12,7 +12,7 @@ class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
     context_object_name = 'products'
-    paginate_by = 5  # 5 элементов на странице
+    paginate_by = 6  # 5 элементов на странице
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
