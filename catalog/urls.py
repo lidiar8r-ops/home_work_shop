8f5ddd705c1f11073817e6243c74d198dbb0fc80
Рыбина Лидия <lidiar@rambler.ru>
2026-03-03 20:52:04 +0500

    update files

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import products_list, products_detail
+from catalog.views import ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     # path("", home, name="home"),
     # path("contacts/", contacts, name="contacts"),
-    path("", products_list, name='products_list'),
-    path("catalog/<int:pk>/", products_detail, name='products_detail'),
+    path("", ProductListView.as_view(), name='products_list'),
+    path("catalog/<int:pk>/", ProductDetailView.as_view(), name='products_detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -63,12 +63,12 @@ class ProductDetailView(DetailView):
 #         "products": page_obj,  # Теперь передаем сам объект пагинации
 #         "is_paginated": paginator.num_pages > 1
 #     }
-#     return render(request, 'products_list.html', context)
+#     return render(request, 'product_list.html', context)
 #
 #
 # def products_detail(request, pk):
 #     product = get_object_or_404(Product, pk=pk)
 #     context = {"product": product}
-#     return render(request, 'products_detail.html', context)
+#     return render(request, 'product_detail.html', context)
 
 
