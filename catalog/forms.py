85a3a21dcd9ba2e64e8db5851f3ab35dc0d746cd
Рыбина Лидия <lidiar@rambler.ru>
2026-04-12 22:49:10 +0500

    add validation  forms.py

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,23 @@
-from django.forms import ModelForm, forms
+from django.forms import ModelForm, forms, BooleanField
+from prompt_toolkit.validation import ValidationError
 
 from catalog.models import Product
 
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
 
-class ProductForm(ModelForm):
+
+dict_worlds = ["казино", "криптовалюта", "крипта", "биржа", "дешево", "бесплатно", "обман", "полиция", "радар", ]
+
+
+class ProductForm(StyleFormMixin,ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
@@ -11,3 +25,16 @@ class ProductForm(ModelForm):
         #     "name": forms.TextInput(attrs={"class": "form-control"}),
         #
         # }
+
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        name_l = name.lower()
+        if name_l in  dict_worlds:
+            raise forms.ValidationError("Нельзя использовать это слово в названиях продуктов")
+
+    def clean_description(self):
+        description = self.cleaned_data['description']
+        description_l = description.lower()
+        if description_l in dict_worlds :
+            raise forms.ValidationError("Нельзя использовать это слово в описаниях продуктов")
