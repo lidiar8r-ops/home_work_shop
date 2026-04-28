723e7ccf496bbd7a2a6e816e22e872ffe4b3a313
Рыбина Лидия <lidiar@rambler.ru>
2026-04-28 22:05:38 +0500

    update users/urls

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from tempfile import template
 
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from catalog.apps import CatalogConfig
@@ -11,5 +11,6 @@ from users.apps import UsersConfig
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("login/", LoginView.as_view(template_name="login.html") ),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout" ),
 ]
