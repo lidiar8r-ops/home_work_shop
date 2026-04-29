8355236f275d72f42bbb00d08e9adf1034f4b890
Рыбина Лидия <lidiar@rambler.ru>
2026-04-29 23:01:38 +0500

    update settings and views

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
-from django.urls import reverse_lazy
+import secrets
+
+from django.core.mail import send_mail
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView
 
+from config.settings import EMAIL_HOST_USER
 from users.forms import UserRegisterForm
 from users.models import User
 
@@ -13,3 +18,19 @@ class UserCreateView(CreateView):
     def form_valid(self, form):
         user = form.save()
         user.is_active = False
+        token = secrets.token_hex(16)
+        host = self.request.get_host()
+        url = f"http://{host}/users/email-confirm/{token}"
+        send_mail(
+            subject="Подтверждение пояты",
+            message=f"Привет! Перейди по ссылке для подтверждения почты {url}",
+            from_email=EMAIL_HOST_USER,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
+
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    return redirect(reverse("users:login"))
