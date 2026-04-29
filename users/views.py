10677222542548020bc56d2558d50765bd9dce1f
Рыбина Лидия <lidiar@rambler.ru>
2026-04-29 23:22:56 +0500

    update urls and views for verifikation

--- users/views.py
+++ users/views.py
@@ -19,10 +19,12 @@ class UserCreateView(CreateView):
         user = form.save()
         user.is_active = False
         token = secrets.token_hex(16)
+        user.token = token
+        user.save()
         host = self.request.get_host()
         url = f"http://{host}/users/email-confirm/{token}"
         send_mail(
-            subject="Подтверждение пояты",
+            subject="Подтверждение почты",
             message=f"Привет! Перейди по ссылке для подтверждения почты {url}",
             from_email=EMAIL_HOST_USER,
             recipient_list=[user.email],
@@ -33,4 +35,5 @@ class UserCreateView(CreateView):
 def email_verification(request, token):
     user = get_object_or_404(User, token=token)
     user.is_active = True
+    user.save()
     return redirect(reverse("users:login"))
